320d58cb37b53ecc98492b6b3f3d8dd727b5cf10
amazonclone <bharathsolanki612@gmail.com>
2024-10-09 00:38:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import FeaturesSection from "./Components/FeaturesSection";
+import Footer from "./Components/Footer";
+import HeroSection from "./Components/HeroSection";
+import Navbar from "./Components/Navbar";
+import Pricing from "./Components/Pricing";
+import Testimonials from "./Components/Testimonials";
+import WorkFlow from "./Components/WorkFlow";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeaturesSection />
+        <WorkFlow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
